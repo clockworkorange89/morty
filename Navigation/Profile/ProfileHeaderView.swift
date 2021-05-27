61bf64288a640999961166fdd3c7334d2a6ff7a0
Алексей Маяков <aleksei@Mac-mini-Aleksej.local>
2021-05-27 22:14:30 +0300

    Correct mistakes

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class  ProfileHeaderView: UIView {
-  private let image  = UIImageView()
-  private let name =  UITextField()
-  private let status = UILabel()
-  private let myButton  = UIButton()
-  private let addText = UITextField()
-  private var  statusText  = ""
- 
+    private let image  = UIImageView()
+    private let name =  UILabel()
+    private let status = UILabel()
+    private let myButton  = UIButton()
+    private let addText = UITextField()
+    private var  statusText  = ""
+    
     override init(frame:CGRect){
         super.init(frame:frame)
         setupUI()
@@ -32,15 +32,6 @@ class  ProfileHeaderView: UIView {
         addSubview(status)
         addSubview(myButton)
         addSubview(addText)
-    }
-    override func layoutSubviews() {
-        configurationFrames()
-    }
-    
-    func configurationFrames() {
-        // Переменная для сохранения значения отступа свеху
-       //let safeSizeTop = image.safeAreaInsets.top + 16
-        image.frame = CGRect(x:self.safeAreaInsets.right + 16 ,y: self.safeAreaInsets.top + 16  ,width: 100,height: 100)
         image.image = UIImage(named: "morty")
         image.layer.borderWidth = 3
         image.layer.borderColor = .init(red: 255, green: 255, blue: 255, alpha: 1)
@@ -48,35 +39,63 @@ class  ProfileHeaderView: UIView {
         image.layer.masksToBounds = true
         
         
-        name.frame = CGRect(x:self.safeAreaInsets.right + 130,y:self.safeAreaInsets.top + 27,width: 140,height: 15)
         name.text = "Evil Morty"
         name.font = .boldSystemFont(ofSize: 18)
         name.textColor = .black
         
         
-        myButton.frame = CGRect(x:self.safeAreaInsets.right + 16,y:self.safeAreaInsets.top + 132,width: 382,height: 50)
         myButton.backgroundColor = .systemIndigo
         myButton.setTitle("Show status", for: .normal)
         myButton.layer.cornerRadius = 4
         myButton.layer.cornerRadius = 15
-        myButton.layer.backgroundColor = .init(red: 0, green: 100, blue: 255, alpha: 2)
+        myButton.backgroundColor = .systemBlue
         myButton.setTitleColor(.white, for: .normal)
         myButton.addTarget(self, action: #selector(doOnButton), for: .touchUpInside)
         
-        status.frame = CGRect(x:self.safeAreaInsets.right + 130,y:self.safeAreaInsets.top + 72 ,width: 190,height: 15)
         status.text = "Waiting for something..."
         status.font = UIFont(name: "regular", size: 14)
         status.textColor = .gray
         
-        addText.frame =  CGRect(x:safeAreaInsets.right + 132,y:self.safeAreaInsets.top + 90,width: 265,height: 40)
+        status.text = "Waiting for something..."
+        status.font = UIFont(name: "regular", size: 14)
+        status.textColor = .gray
+        
         addText.text = "aaaaaas"
         addText.layer.cornerRadius = 12
         addText.font = UIFont(name: "regular", size: 15)
         addText.layer.borderWidth = 1
-        addText.layer.backgroundColor = .init(red: 255, green: 255, blue: 255, alpha: 1)
+        addText.backgroundColor = .init(red: 255, green: 255, blue: 255, alpha: 1)
         addText.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
         
-        }
+        
+    }
+    override func layoutSubviews() {
+        configurationFrames()
+    }
+    
+    func configurationFrames() {
+        // Переменная для сохранения значения отступа свеху
+        //let safeSizeTop = image.safeAreaInsets.top + 16
+        image.frame = CGRect(x:self.safeAreaInsets.right + 16 ,y: self.safeAreaInsets.top + 16  ,width: 100,height: 100)
+        
+        
+        
+        name.frame = CGRect(x:self.safeAreaInsets.right + 140,y:self.safeAreaInsets.top + 27,width: 140,height: 15)
+        
+        
+        
+        myButton.frame = CGRect(x:self.safeAreaInsets.right + 16,y:self.safeAreaInsets.top + 132,width: 382,height: 50)
+        
+        
+        status.frame = CGRect(x:self.safeAreaInsets.right + 140,y:self.safeAreaInsets.top + 66 ,width: 190,height: 15)
+        
+        addText.frame =  CGRect(x:safeAreaInsets.right + 132,y:self.safeAreaInsets.top + 87,width: 265,height: 40)
+        
+// Сдвинуть текст  в лево с texField
+        addText.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: addText.frame.height))
+        addText.leftViewMode = .always
+        // Конец
+    }
     
     @objc func doOnButton(){
         print(status.text!)
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,31 +11,31 @@ import UIKit
 class  ProfileViewController: UIViewController {
     // Вносить изменения в UIView
     //Создаётся view   выше методов
-    private let vi = ProfileHeaderView()
+    private let viMorty = ProfileHeaderView()
     override func viewDidLoad() {
         super.viewDidLoad()
         //изменить цвет
         view.backgroundColor = .lightGray
         
-       // vi.backgroundColor = .red
+        // vi.backgroundColor = .red
         //Добавить vi  в качестве subview
-        vi.layer.shadowOffset = .init(width: 4, height: 4)
-        vi.layer.shadowRadius = 4
-        vi.layer.shadowColor = .init(red: 0, green: 0, blue: 0, alpha: 0.5)
-        vi.layer.shadowOpacity = 0.7
-        self.view.addSubview(vi)
+        viMorty.layer.shadowOffset = .init(width: 4, height: 4)
+        viMorty.layer.shadowRadius = 4
+        viMorty.layer.shadowColor = .init(red: 0, green: 0, blue: 0, alpha: 0.5)
+        viMorty.layer.shadowOpacity = 0.7
+        self.view.addSubview(viMorty)
         //self.viewWillLayoutSubviews()
         print(self.view.frame)
-        print(vi.bounds)
+        print(viMorty.bounds)
         
-   }
+    }
     // Доьавление размера view
     override func viewWillLayoutSubviews(){
         super.viewWillLayoutSubviews()
-        vi.frame = view.frame
+        viMorty.frame = view.frame
         print(self.view.frame)
-        print(vi.bounds)
+        print(viMorty.bounds)
     }
-   
+    
 }
 
