ecdcccfe8b93ce30d2dde5a7e141a8bf15b05160
Алексей Маяков <aleksei@Mac-mini-Aleksej.local>
2021-05-28 09:54:17 +0300

    done

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class  ProfileHeaderView: UIView {
-  
     
-    @IBOutlet weak var avatarImageView: UIImageView!
-    @IBOutlet weak var fullNameLabel: UILabel!
-    @IBOutlet weak var statusLabel: UILabel!
     
-    @IBOutlet weak var statusTextField: UITextField!
-    
-    @IBOutlet weak var setStatusButton: UIButton!
+    @IBOutlet weak private var avatarImageView: UIImageView!
+    @IBOutlet weak private  var fullNameLabel: UILabel!
+    @IBOutlet weak private  var statusLabel: UILabel!
+    @IBOutlet weak private  var statusTextField: UITextField!
+    @IBOutlet weak private  var setStatusButton: UIButton!
     private var  statusText  = ""
-    private(set) lazy var newButton: UIButton = {
+    private(set) var newButton: UIButton = {
         let newButton = UIButton()
         newButton.backgroundColor = .red
         newButton.setTitle("Push", for: .normal)
@@ -28,40 +26,25 @@ class  ProfileHeaderView: UIView {
     
     
     
-override func awakeFromNib() {
-    configurationFrames()
-   
-    
-    }
-    
-
-
- /*
-    override init(frame:CGRect){
-        super.init(frame:frame)
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configurationFrames()
+        
         
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
-*/
-    
-
-    override func layoutSubviews() {
-         addSubview(newButton)
-        newButton.translatesAutoresizingMaskIntoConstraints = false
-        newButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
-         newButton.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        newButton.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor , constant: 0).isActive = true
-        newButton.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
-       // newButton.bottomAnchor.constraint(equalTo: ProfileHeaderView.safe, constant: 1)
-        newButton.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor).isActive = true
-    }
     
     func configurationFrames() {
-  
+        addSubview(newButton)
+        newButton.translatesAutoresizingMaskIntoConstraints = false
+        newButton.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        newButton.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor , constant: 0).isActive = true
+        newButton.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
+        // newButton.bottomAnchor.constraint(equalTo: ProfileHeaderView.safe, constant: 1)
+        newButton.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        
         avatarImageView.layer.borderWidth = 3
         avatarImageView.layer.borderColor = .init(red: 255, green: 255, blue: 255, alpha: 1)
         avatarImageView.layer.cornerRadius = 50
@@ -73,7 +56,7 @@ override func awakeFromNib() {
         fullNameLabel.textColor = .black
         
         
-
+        
         setStatusButton.backgroundColor = .systemIndigo
         setStatusButton.setTitle("Show status", for: .normal)
         setStatusButton.layer.cornerRadius = 4
@@ -82,19 +65,22 @@ override func awakeFromNib() {
         setStatusButton.setTitleColor(.white, for: .normal)
         setStatusButton.addTarget(self, action: #selector(doOnButton), for: .touchUpInside)
         
-     
+        
         statusLabel.text = "Waiting for something..."
         statusLabel.font = UIFont(name: "regular", size: 14)
         statusLabel.textColor = .gray
-      
+        
         statusTextField.text = "aaaaaas"
         statusTextField.layer.cornerRadius = 12
         statusTextField.font = UIFont(name: "regular", size: 15)
         statusTextField.layer.borderWidth = 1
         statusTextField.layer.backgroundColor = .init(red: 255, green: 255, blue: 255, alpha: 1)
+        statusTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: statusTextField.frame.height))
+        statusTextField.leftViewMode = .always
         statusTextField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
-  
-  }
+        
+        
+    }
     
     @objc func doOnButton(){
         print(statusLabel.text!)
@@ -106,5 +92,5 @@ override func awakeFromNib() {
         statusText = textField.text ?? "empty"
     }
     
-  
+    
 }
